8067b8a0b2f7c58c2ac925ecfe1457a083dbb883
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-10-30 20:29:38 -0300

    getValuesUser method class class UserController {
    Instancia a class User e retorna o objeto com seus atributos

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,4 +8,40 @@ class UserController {
         //this.onSubmitUser();
         
     }
+
+
+    //Retorna o objeto com os valores do usuário
+    getValuesUser() {
+
+        const user = {}; //JSON USER
+
+        //Utiliza-se o spred criando um Array para funcionalidade do forEach
+        [...this._formEl.elements].forEach(e => {
+
+            if (e.name == "gender") {
+
+                if (e.checked) { user[e.name] = e.value; }
+
+            } else if (e.name == "admin") {
+
+                user[e.name] = e.checked;
+
+            } else {
+                user[e.name] = e.value;
+            }
+
+        });
+
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+        );
+
+    } // Close getValuesUser
 }
