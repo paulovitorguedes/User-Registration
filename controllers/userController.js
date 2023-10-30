7d048c87062a3cf8006a06819e98c108ed3e697e
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-10-30 20:14:37 -0300

    create userController.js
    Class UserController para regra de negócio de Users

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+class UserController {
+
+    constructor(formId, tableId) {
+
+        this._formEl = document.getElementById(formId);
+        this._tableEl = document.getElementById(tableId);
+
+        //this.onSubmitUser();
+        
+    }
+}
